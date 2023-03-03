e37784e8c1b2098fe3b97d35fc2ecf6ddc90c018
cuvar <luca-mueller@gmx.net>
2023-03-03 10:38:02 +0100

    feat: initial placement

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,54 @@
 "use client";
 
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./index.css";
 
 interface IProps {
   children: React.ReactNode;
+  actions: React.ReactNode;
 }
+
 export default function SwiperAction(props: IProps) {
+  const [swiping, setSwiping] = useState(false);
+  const [deltaX, setDeltaX] = useState(0);
+  const [startX, setStartX] = useState(0);
+  const swiperRef = useRef(null);
+
+  function handleMouseDown(ev: React.MouseEvent<Element, MouseEvent>) {
+    setSwiping(true);
+    setDeltaX(0);
+    setStartX(ev.clientX);
+    // console.log(ev.clientX);
+    // console.log("mouse down");
+  }
+
+  function handleMouseUp(ev: React.MouseEvent<Element, MouseEvent>) {
+    setSwiping(false);
+    // console.log("mouse up");
+  }
+
+  function handleMouseMove(ev: React.MouseEvent<Element, MouseEvent>) {
+    if (swiping) {
+      const delta = ev.clientX - startX;
+      setDeltaX(delta);
+      console.log(delta);
+    }
+  }
+
   return (
-    <div>
-      <div>{props.children}</div>
+    <div className="w-full">
+      <div
+        className="relative w-full debug2"
+        onMouseDown={(e) => handleMouseDown(e)}
+        onMouseUp={(e) => handleMouseUp(e)}
+        onMouseMove={(e) => handleMouseMove(e)}
+        ref={swiperRef}
+      >
+        <div className="item w-full z-1 absolute left-0">{props.children}</div>
+        <div className="actions flex-row z-0 absolute right-0">
+          {props.actions}
+        </div>
+      </div>
     </div>
   );
 }
--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,18 +1,30 @@
 import SwiperAction from "swiper-action";
 
 function App() {
+  const actions = (
+    <>
+      <button className="action debug" onClick={(e) => console.log("action 1")}>
+        Action 1
+      </button>
+      <button className="action debug" onClick={(e) => console.log("action 2")}>
+        Action 2
+      </button>
+    </>
+  );
   return (
     <div className="w-screen h-screen bg-sky-700 flex justify-center items-center">
       <div className="w-1/2 flex flex-col space-y-2">
-        <SwiperAction>
-          <div className="bg-white rounded-lg py-3 px-2">First Item</div>
+        <SwiperAction actions={actions}>
+          <div className="bg-white flex items-center rounded-lg py-3 px-2">
+            First Item
+          </div>
         </SwiperAction>
-        <SwiperAction>
+        {/* <SwiperAction actions={actions}>
           <div className="bg-white rounded-lg py-3 px-2">Second Item</div>
         </SwiperAction>
-        <SwiperAction>
+        <SwiperAction actions={actions}>
           <div className="bg-white rounded-lg py-3 px-2">Third Item</div>
-        </SwiperAction>
+        </SwiperAction> */}
       </div>
     </div>
   );
