2b4fd59c109667ce37457d018d1632a574a004d0
cuvar <luca-mueller@gmx.net>
2023-06-11 11:49:45 +0200

    feat: collapse on click

--- src/Action.tsx
+++ src/Action.tsx
@@ -1,12 +1,26 @@
 "use client";
-import React from "react";
-import { ActionProps } from "./types";
+import React, { useRef } from "react";
+import { ActionProps, InteractionEvent } from "./types";
 
 export default function Action(props: ActionProps) {
+  const buttonRef = useRef(null);
+
+  function handleClick(e: InteractionEvent) {
+    if (buttonRef.current) {
+      // @ts-ignore
+      const parent = buttonRef.current.parentElement;
+      parent.style.transition = `width 0.2s`;
+      parent.style.width = `0`;
+      // parent.style.transition = ``;
+    }
+    props.action(e);
+  }
+
   return (
     <button
       className="action debug w-full h-full"
-      onClick={(e) => props.action(e)}
+      onClick={(e) => handleClick(e)}
+      ref={buttonRef}
     >
       <div>{props.children}</div>
     </button>
